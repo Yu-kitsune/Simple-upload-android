2c4968a6da0e341e47f78db36360ff5f29abcc47
DaiAiTienTon <99729215+DaiAiTienTon@users.noreply.github.com>
2025-12-10 09:53:54 +0700

    day doan dau

---
+++ app/src/main/java/com/example/ai/LoadingActivity.kt
@@ -0,0 +1,26 @@
+package com.example.ai
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ai.databinding.ActivityLoadingBinding
+
+class LoadingActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoadingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoadingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
